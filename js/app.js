27a76542fe0fa68119cf386af81fc0f5940c792f
jairorjuela <genius85862@gmail.com>
2018-04-02 22:17:51 -0500

    Picas 1_css

---
+++ js/app.js
@@ -0,0 +1,50 @@
+// body {
+//   font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+//   background-color: #D8D8D8;
+//   max-width: 50%;
+//   margin: auto;
+// }
+//
+// h1 {
+//   font-size: 50px;
+//   line-height: 60px;
+//   text-align: center;
+//   color: #333333;
+// }
+//
+// h2 {
+//   font-size: 20px;
+//   line-height: 20px;
+//   text-align: center;
+//   color: #333333;
+// }
+//
+// input {
+//     width: 100%;
+//     padding: 8px 8px 8px;
+//     box-sizing: border-box;
+//     border: 5px solid #F7BE81;
+//     border-radius: 15px;
+//     background-color: #E6E6E6;
+//     text-align: center;
+//     font-size: 20px;
+//     position: relative;
+// }
+//
+//
+// table {
+//   position: relative;
+//
+//
+//   top: auto;
+//   border-collapse: collapse;
+//   border: 5px solid red;
+//   width: 50%;
+// }
+//
+// th {
+//   padding-top: 12px;
+//   padding-bottom: 12px;
+//   text-align: center;
+//   color: 333333;
+// }
