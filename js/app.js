22f5be9e896aa124bae024b3f47eb3e5c01dfc79
jairorjuela <genius85862@gmail.com>
2018-04-05 22:45:40 -0500

    Picas 2_css

--- js/app.js
+++ js/app.js
@@ -1,50 +1,17 @@
-// body {
-//   font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-//   background-color: #D8D8D8;
-//   max-width: 50%;
-//   margin: auto;
-// }
-//
-// h1 {
-//   font-size: 50px;
-//   line-height: 60px;
-//   text-align: center;
-//   color: #333333;
-// }
-//
-// h2 {
-//   font-size: 20px;
-//   line-height: 20px;
-//   text-align: center;
-//   color: #333333;
-// }
-//
-// input {
-//     width: 100%;
-//     padding: 8px 8px 8px;
-//     box-sizing: border-box;
-//     border: 5px solid #F7BE81;
-//     border-radius: 15px;
-//     background-color: #E6E6E6;
-//     text-align: center;
-//     font-size: 20px;
-//     position: relative;
-// }
-//
-//
-// table {
-//   position: relative;
-//
-//
-//   top: auto;
-//   border-collapse: collapse;
-//   border: 5px solid red;
-//   width: 50%;
-// }
-//
-// th {
-//   padding-top: 12px;
-//   padding-bottom: 12px;
-//   text-align: center;
-//   color: 333333;
-// }
+$(document).ready(function(){
+  $('table').addClass('hide')
+// validacion de caracteres
+  $('textarea').on('keypress', function(event){
+    key = event.keyCode;
+    if(key === 13){
+      console.log("enter");
+      var number = /^[0-9]{1,4}$/;
+      // if(($('textarea').value.match(number)){
+      //   var cont = $(this).val().length;
+      //   if(cont <= 3){
+      //     $('span').css("color","red");
+      //   }
+      // });
+    };
+  });
+});
