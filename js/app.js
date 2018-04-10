cc6d0762b52706f41b66121ff2456912177fef43
jairorjuela <genius85862@gmail.com>
2018-04-09 23:28:17 -0500

    Picas 4

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 //generacion de numero para jugar
-
   function shuffle(array) {
     for(var j, x, i = array.length; i; j = parseInt(Math.random() * i), x =   array[--i], array[i] = array[j], array[j] = x);
     return array;
@@ -44,37 +43,42 @@
           }
         }
 
-//picas y fijas
-
+//conteo de picas y fijas
         var x = 0;
         var y = 0;
 
-if(jairo != null){
+        if(jairo != null){
 
-var newJairo = jairo.map(Number);
-
-
-if(newJairo[0] === random[0]){
-  x += 1;
-}if((newJairo[0] === random[1]) || (newJairo[0] === random[2]) || (newJairo[0] === random[3])){
-  y += 1;
-}if(newJairo[1] === random[1]){
-  x += 1;
-}if((newJairo[1] === random[0]) || (newJairo[1] === random[2]) || (newJairo[1] === random[3])){
-  y += 1;
-}if(newJairo[2] === random[2]){
-  x += 1;
-}if((newJairo[2] === random[0]) || (newJairo[2] === random[1]) || (newJairo[2] === random[3])){
-  y += 1;
-}if(newJairo[3] === random[3]){
-  x += 1;
-}if((newJairo[3] === random[0]) || (newJairo[3] === random[1]) || (newJairo[3] === random[2])){
-  y += 1;
-}
+          var newJairo = jairo.map(Number);
 
 
-console.log("pica " + y);
-console.log("fija " + x);
+          if(newJairo[0] === random[0]){
+            x += 1;
+          }if((newJairo[0] === random[1]) || (newJairo[0] === random[2]) || (newJairo[0] === random[3])){
+            y += 1;
+          }if(newJairo[1] === random[1]){
+            x += 1;
+          }if((newJairo[1] === random[0]) || (newJairo[1] === random[2]) || (newJairo[1] === random[3])){
+            y += 1;
+          }if(newJairo[2] === random[2]){
+            x += 1;
+          }if((newJairo[2] === random[0]) || (newJairo[2] === random[1]) || (newJairo[2] === random[3])){
+            y += 1;
+          }if(newJairo[3] === random[3]){
+            x += 1;
+          }if((newJairo[3] === random[0]) || (newJairo[3] === random[1]) || (newJairo[3] === random[2])){
+            y += 1;
+          }
+//llenado de tabla
+          var numer = newJairo.join("")
+
+          $('tbody').append('<tr>' + '<td>' + numer + '</td>' + '<td>' + y + '</td>' + '<td>' + x + '</td>' + '</tr>');
+
+          if(x === 4){
+            console.log("ganaste");
+            
+          };
+
 
 
 
